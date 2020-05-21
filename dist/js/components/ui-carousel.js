8404c54f2ae90a0b6815e39843844472e0c43476
Blari <Blarior@gmail.com>
2020-05-21 15:56:12 +0300

    Add ui cards, ui carousel page

--- dist/js/components/ui-carousel.js
+++ dist/js/components/ui-carousel.js
@@ -1,7 +1,62 @@
 $(function(){
-  function pageLoad(){
-    $('.carousel').carousel()
+  function pageLoad() {
+    $('.carousel').carousel();
+
+    $('.slick').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+
+
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:3
+        },
+        1000:{
+          items:5
+        }
+      }
+    })
   }
+
   pageLoad();
   SingApp.onPageLoad(pageLoad);
 });
+
