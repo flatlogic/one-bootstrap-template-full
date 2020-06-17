b659b295b08180e49bc39bfcfaee85a46c619468
Blari <Blarior@gmail.com>
2020-06-17 16:51:48 +0300

    breadcrumb, titles (h1) for all pages

--- dist/js/extra/timeline.js
+++ dist/js/extra/timeline.js
@@ -1,6 +1,6 @@
 $(function(){
     function initGmap(){
-        var map = new GMaps({
+        let map = new GMaps({
             el: '#gmap',
             lat: -37.813179,
             lng: 144.950259,
@@ -21,6 +21,23 @@ $(function(){
             });
         }, 3000);
     }
+
+    $(document).ready(function(){
+        let my_posts = $("[rel=tooltip]");
+
+        let size = $(window).width();
+        for(i=0;i<my_posts.length;i++){
+            the_post = $(my_posts[i]);
+
+            if(the_post.hasClass('invert') && size >=767 ){
+                the_post.tooltip({ placement: 'left'});
+                the_post.css("cursor","pointer");
+            }else{
+                the_post.tooltip({ placement: 'rigth'});
+                the_post.css("cursor","pointer");
+            }
+        }
+    });
 
     function pageLoad(){
         initGmap();
